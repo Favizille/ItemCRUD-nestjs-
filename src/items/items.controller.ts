2988d9b89bda6b9eddefd463a11b2cf53def5549
Favizille <89010415+Favizille@users.noreply.github.com>
2023-03-20 14:08:06 +0100

    connecting to mongosDB, added items Module and linked some methods from ItemService to ItemController

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Get, Post, Put, Delete} from '@nestjs/common';
 import { Body, Param } from '@nestjs/common/decorators/http/route-params.decorator';
 import { CreateItemDto } from './dto/create-items.dto';
+import { Item } from './interface/item.interface';
+import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
+
+    constructor( private readonly itemsService:ItemsService){}
     @Get()
-    getAll():string{
-        return "all items";
+    getAll(): Item[]{
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
-    getOneItem(@Param('id') id:number) : string{
-        return `Item ${id}`;
+    getOneItem(@Param('id') id:string) : Item{
+        return this.itemsService.findbyId(id);
     }
 
     @Post()
@@ -20,7 +24,7 @@ export class ItemsController {
     }
 
     @Put(':id')
-    modifyItem(@Param('id') id:number, @Body() updateItemDto:CreateItemDto){
+    modifyItem(@Param('id') id:string, @Body() updateItemDto:CreateItemDto){
         return `Item ${id} -Name: ${updateItemDto.name}`;
     }
 
